de2d4eadc55c1773318819cfc58677dcd2fc6cb4
JUkhan <jasim.uddin.khan@gmail.com>
2021-04-26 10:19:53 +0600

    v0.2.5

--- src/hooks/useStoreEffecct.ts
+++ src/hooks/useStoreEffecct.ts
@@ -3,13 +3,14 @@ import { useStore } from "./useStore";
 import { useIsomorphicLayoutEffect } from "../utils/useIsomorphicLayoutEffect";
 import { Actions } from "../actions";
 import { Action } from "../action";
+import { MonoStore } from "../store";
 
-export function useStoreEffect(
-  stream$: (action$: Actions) => Observable<Action>
+export function useStoreEffect<S = any>(
+  stream$: (action$: Actions, store: MonoStore<S>) => Observable<Action>
 ) {
   const store = useStore();
   useIsomorphicLayoutEffect(() => {
-    const sub = stream$(store.action$).subscribe((action) =>
+    const sub = stream$(store.action$, store).subscribe((action) =>
       store.dispatch(action)
     );
     return () => sub?.unsubscribe();
